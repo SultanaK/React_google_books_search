581acebad5b585ced040615f45b1adf06f750139
Sultana <kr.shampa@yahoo.com>
2019-01-20 12:30:58 -0600

    add app

---
+++ client/build/precache-manifest.3bf7165dfe987ec58116f04d40c2dbb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6858aed0b60c1ca6f0a",
+    "url": "/React_google_books_search/static/js/runtime~main.c6858aed.js"
+  },
+  {
+    "revision": "ce5f44d8e1f63d3df160",
+    "url": "/React_google_books_search/static/js/main.ce5f44d8.chunk.js"
+  },
+  {
+    "revision": "90f21e8c38fda7773792",
+    "url": "/React_google_books_search/static/js/1.90f21e8c.chunk.js"
+  },
+  {
+    "revision": "ce5f44d8e1f63d3df160",
+    "url": "/React_google_books_search/static/css/main.c26e3415.chunk.css"
+  },
+  {
+    "revision": "1ea2c085c4e09321401b235f9e6348a6",
+    "url": "/React_google_books_search/index.html"
+  }
+];
